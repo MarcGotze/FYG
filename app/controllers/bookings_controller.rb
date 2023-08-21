d9086a0ca4ce89a23399378014156c4c0e5326c8
Eleonore Baillot <eleonorebaillot@gmail.com>
2023-08-21 18:02:31 +0200

    booking controller new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+  # before-action :set_booking, only: %i[destroy]
+
+  def new
+    @venue = Venue.find(params[:venue_id])
+    @booking = Booking.new
+  end
+
+  def create
+    if @booking.save
+      redirect_to 'history_user_venue_bookings_path'
+    else
+    end
+  end
+
+  def history
+  end
+
+  def destroy
+  end
+
 end
