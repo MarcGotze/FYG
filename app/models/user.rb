1ccbcb125edc0235c83fd88c7d237f1d2a881d67
Eleonore Baillot <eleonorebaillot@gmail.com>
2023-08-22 15:32:06 +0200

    new routs and destroy dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   ROLE = ["musician", "owner"]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :venues, dependent: :destroy
   validates :email, :password, :username, :role, presence: true
 end
