9bb4e90ae4856321b5d19c744c0c712f1cd2f21b
Eleonore Baillot <eleonorebaillot@gmail.com>
2023-08-22 11:15:05 +0200

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :bookings
+  validates :email, :password, :username, :role, presence: true
+
 end
