970a0beba61941ad989372fabfe942ccb35e4094
Marc Gotze marc.gotze@gmail.com <marc.gotze@gmail.com>
2023-08-25 11:51:32 +0200

    Avatar completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :bookings, dependent: :destroy
   has_many :venues, dependent: :destroy
-  validates :email, :password, :username, :role, presence: true
+  validates :email, :password, :username, :avatar, presence: true
+  has_one_attached :avatar
 end
